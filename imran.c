a28b765ab95b0c7dd8853df978754519ac11a069
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-02 16:29:21 +0530

    Update imran.c

--- imran.c
+++ imran.c
@@ -2,12 +2,19 @@
 #include<conio.h>
 void main()
 {
-    int number;
-clrscr();
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-
-    (number % 2 == 0) ? printf("%d is even.", number) : printf("%d is odd.", number);
-
-    getch();
+  char ch;
+ clrscr();
+  printf("Input a character\n");
+  scanf("%c", &ch);
+ 
+  if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')) {
+    if (ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch== 'u' || ch=='U')
+      printf("%c is a vowel.\n", ch);
+    else
+      printf("%c is a consonant.\n", ch);
+  }
+  else
+    printf("%c is neither a vowel nor a consonant.\n", ch);
+ 
+  getch();
 }
