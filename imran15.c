a7f190f91564e95592ef9a29b2a59a4bb7d5b8e2
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-05 14:10:21 +0530

    Update and rename imran14.c to imran15.c

---
+++ imran15.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h> 
+#include<conio.h>
+   
+void main(){  
+    int counter; 
+    printf("Even numbers between 1 to 100\n");  
+    /* 
+     * Initialize counter with 1, and increment it in every iteration.
+     * For every value of counter check whether it is even number or
+     * not and print it accordingly 
+     */ 
+    for(counter = 1; counter <= 100; counter++) {  
+        /* Even numbers are divisible by 2 */ 
+        if(counter%2 == 0) { 
+            /* counter is even, print it */
+            printf("%d ", counter);  
+        }  
+    }  
+   
+    getch(); 
+} 
+
