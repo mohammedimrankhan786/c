db1a41761364db21b376165ea62fff4672531b02
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-05 13:39:22 +0530

    Update and rename imran6.c to imran7.c

---
+++ imran7.c
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include<conio,h>
+void main()
+using namespace std;
+int main() {
+    int i, n;
+    cin >> n;
+    for (i=0; i<n; i++) {
+        cout << "hello world\n";
+    }
+    getch();
+}
