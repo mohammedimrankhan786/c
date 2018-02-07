40439e6f760c3209783fb08c7c36e8b8a366970f
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-07 14:56:16 +0530

    Update imran25.c

--- imran25.c
+++ imran25.c
@@ -1,25 +1,35 @@
-#include <stdio.h>
-#include<conio.h> 
-int findMaximum(int arr[], int low, int high)
-{
-   int max = arr[low];
-   int i;
-   for (i = low; i <= high; i++)
-   {
-       if (arr[i] > max)
-          max = arr[i];
-   }
-   return max;
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <time.h>
+#include<conio.h>
+std::string gen_random() {
+    size_t len = rand() % 25 + 5;
+
+    std::string x;
+
+    std::generate_n(std::back_inserter(x), len, rand);
+    return x;
 }
- 
-/* Driver program to check above functions */
-int main()
-{
-   int arr[] = {1, 30, 40, 50, 60, 70, 23, 20};
-   int n = sizeof(arr)/sizeof(arr[0]);
-   printf("The maximum element is %d", findMaximum(arr, 0, n-1));
-   getchar();
-   getch();
+
+static const int num = 10000000;
+
+int main(){
+    std::vector<std::string> strings;
+
+    std::generate_n(std::back_inserter(strings), num, gen_random);
+
+    clock_t start = clock();
+    std::sort(strings.begin(), strings.end());
+    clock_t ticks = clock() - start;
+
+    std::cout << ticks / (double)CLOCKS_PER_SEC;
+
+    getch();
 }
 
 
