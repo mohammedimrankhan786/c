fcd1e589c9d27b166c57c0e26d88935383357edf
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-07 15:09:45 +0530

    Update and rename imran26.c to imran27.c

---
+++ imran27.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+public static boolean isNumeric(String str)  
+{  
+  try  
+  {  
+    double d = Double.parseDouble(str);  
+  }  
+  catch(NumberFormatException nfe)  
+  {  
+    return false;  
+  }  
+  return true;  
+}
+ghetch();
+}
+
+
