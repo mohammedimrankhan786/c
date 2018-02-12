e8aed267b3a416dac2036c695b62e50eb542b087
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-12 13:59:27 +0530

    Update and rename 107.c to 108.c

---
+++ 108.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) {
+int a,b,temp;
+scanf("%d %d",&a,&b);
+temp=a;
+a=b;
+b=temp;
+printf("%d %d",a,b);
+	return 0;
+}
+{
+	getch();
+}
+           
