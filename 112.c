d6f47d39ac3de9b0e2b861f55f57e42c71d052aa
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-12 14:14:52 +0530

    Update and rename 111.c to 112.c

---
+++ 112.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[10],i,count=0;
+printf("enter the string");
+scanf("%s",s);
+for(i=0;s[i]='\0';i++)
+{
+if(s[i]==' ')
+count++;
+}
+printf("the total words are%d",count+1);
+}
+{
+	getch();
+}
+
+
+		
+		
+	
