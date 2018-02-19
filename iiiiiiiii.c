dbe3dcc0b196d4f4c2b412e871482bb959781f8f
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-19 13:20:13 +0530

    Create iiiiiiiii.c

---
+++ iiiiiiiii.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    string str = "abcbaa";
+    int k = 3;
+    cout << "Total substrings with exactly "
+         << k <<" distinct characters :"
+         << countkDist(str, k) << endl;
+    return 0;
+}
+{
+getch();
+}
