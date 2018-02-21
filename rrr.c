9d6aecc1a6924ced7f92473b6003425af5a990cc
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-21 14:01:26 +0530

    Create rrr.c

---
+++ rrr.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b;
+  clrscr();
+
+	printf("enter the number:");
+	scanf("%d",&a);
+	b=a+1;
+	printf("%d",b);
+	return 0;
+}
+getch();
+}
