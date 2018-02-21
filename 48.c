bbfef411c6d3b6603c813bacb8e00f62698075f8
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-21 14:15:00 +0530

    Update and rename 47.c to 48.c

---
+++ 48.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[10],i,avg,sum=0,n;
+  printf("enter the total number of digits");
+  scanf("%d",&n);
+  printf("enter the numbers");
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&a[i]);
+  sum=sum+a[i];
+  }
+  avg=sum/n;
+  printf("the average of given numbers is %d",avg);
+}
+getch();
+}
+
