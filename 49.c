7caa0859d0f5642b2984fe4ef29a11bf595900a3
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-21 14:17:21 +0530

    Update and rename 48.c to 49.c

---
+++ 49.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("enter the number:");
+	scanf("%d",&a);
+	printf("enter the number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d",c);
+	return 0;
+}
+getch();
+}
+
