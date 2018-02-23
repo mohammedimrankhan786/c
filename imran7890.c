4c9de2813b95df508879b8b768b14604ad094a86
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-23 16:03:58 +0530

    Update and rename imran654.c to imran7890.c

---
+++ imran7890.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int n,m,p;
+	scanf("%d %d",&n,&m);
+	p=n*m;
+             if(p%2==0)
+             {
+             	printf("even");
+             }
+             else
+             {
+             	printf("odd");
+             }
+	
+	return 0;
+}
+getch();
+}
