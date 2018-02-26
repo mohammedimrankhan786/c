3714090ce0214637e268b44b4e5f80ee1fd38d2e
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-26 13:21:35 +0530

    Update and rename ggggg.c to imran57.c

---
+++ imran57.c
@@ -0,0 +1,19 @@
+
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   int x,y,t;
+    printf("enter the three values");
+   scanf("%d %d",&x,&y);
+   if(a!=0)
+   {
+       t=x;
+       x=y;
+       y=t;
+   }
+   printf("%d %d",x,y);
+    return 0;
+}
