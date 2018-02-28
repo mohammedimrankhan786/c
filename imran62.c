c6314bdbea183873099a227bb2125ff48bbbcae0
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-02-28 14:01:33 +0530

    Update and rename imran61.c to imran62.c

---
+++ imran62.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int str;
+int a;
+printf("enter the string");
+scanf("%d",&str);
+if((str==0)||(str==1))
+{
+printf("binary representation");
+}
+else
+{
+printf("not a binary representation");
+}
+}
+getch();
+}
+
