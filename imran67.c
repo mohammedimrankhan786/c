98788c918cd36a48c243b75cd0e31a13f4a3e160
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-02 15:53:28 +0530

    Update and rename imran66.c to imran67.c

---
+++ imran67.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) 
+{
+	int no;
+	printf("enter the number:");
+	scanf("%d",&no);
+	no+=1;
+	while(no%10!=0)
+	{
+		no=no+1;
+	}
+	printf("\n%d",no);
+	return 0;
+}
+getch();
+}
+
