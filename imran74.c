6a338cf26780296cb434c83ed697286451130a51
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-05 13:13:50 +0530

    Update and rename imran73.c to imran74.c

---
+++ imran74.c
@@ -0,0 +1,14 @@
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float x;
+printf("enter the value");
+scanf("%f",&x);
+printf("round of value is %5.0f",x);
+}
+getch();
+}
