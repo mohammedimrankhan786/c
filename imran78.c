e541f774db0848f3c46a61c97967de712194f980
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-05 13:56:02 +0530

    Update and rename imran77.c to imran78.c

---
+++ imran78.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int num,n;
+  printf("enter a number");
+  scanf("%d",&num);
+  while(num!=0)
+  {
+      do
+      {
+      m=num%13
+      }
+      if(m==0)
+      {
+       printf("multiple of 13");
+       }
+       else
+       {
+       printf("not a multiple of 13");
+       }
+       }
+       
+       }
+getch();
+}
