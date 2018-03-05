42659e001be8830d7d802089956eb5121e21ba4b
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-05 13:12:02 +0530

    Update and rename imran72.c to imran73.c

---
+++ imran73.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) {
+int n,l,r;
+scanf("%d%d%d",&n,&l,&r);
+if((n>=l)&&(n<=r))
+{
+	printf("no is in between the range");
+}
+else
+{
+		printf("no is not in between the range");
+}
+	return 0;
+}
+getch();
+}
