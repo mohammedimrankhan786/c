f82a8fdaba62aa55443d896289917d25e62f1518
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-07 14:15:38 +0530

    Update and rename imran82.c to imran83.c

---
+++ imran83.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,div,mod;
+printf("enter the values");
+scanf("%d%d",&a,&b);
+mod=a%b;
+div=a/b;
+printf("%d\t",div);
+printf("%d\t",mod);
+}
+    
