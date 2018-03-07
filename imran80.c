f7b1985b0d1703dac661183f4b798fddc0fcc377
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-07 14:01:22 +0530

    Update and rename imran79.c to imran80.c

---
+++ imran80.c
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the values: \n");
+scanf("%d",&a);
+while(a!=0)
+{
+b=a%10;
+if(a%2!=0)
+{
+
+    printf("%d \n",b);
+}
+a=a/10;
+}
+return 0;
+}
+getch();
+}
+ 
