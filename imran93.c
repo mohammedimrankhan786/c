d20d1b0a57ff8a1014f1c30eb7394a6a4953aa8a
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-14 15:49:17 +0530

    Update and rename imran.92.c to imran93.c

---
+++ imran93.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int c;
+printf("\n enter the celsius value:");
+scanf("%d",&c);
+printf("\n the kelvin value:%d",c+273);
+return 0;
+}
+getch();
+}
