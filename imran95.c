938c8f2e1276730520eead297b5461e3a7e19818
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-14 15:57:16 +0530

    Update and rename imran94.c to imran95.c

---
+++ imran95.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,p,t;
+float r;
+printf("enter the principal amount");
+scanf("%d",&p);
+printf("enter the time period");
+scanf("%d",&t);
+printf("enter the rate");
+scanf("%f",&r);
+a=p *(1+r*t);
+printf("%d",a);
+}
