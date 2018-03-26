61136ad4abe4fcf63e3691f2331c31e46385b856
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-26 15:54:31 +0530

    Update and rename imran108.c to imran109.c

---
+++ imran109.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,min,c[10];
+for(i=0;i<10;i++)
+{
+scanf("%d",&c[i]);
+}
+min=c[0];
+for(i=0;i<10;i++)
+{
+if(c[i]<min)
+{
+min=c[i];
+}
+
+}
+printf("%d",min);
+return 0;
+});
+}
+getch();
+}
