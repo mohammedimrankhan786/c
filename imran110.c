2cee5f2f0b706d0e4947c346b1d95e71453d3835
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-26 15:58:03 +0530

    Update and rename imran109.c to imran110.c

---
+++ imran110.c
@@ -0,0 +1,17 @@
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum=0,i,a;
+printf("Enter the limit");
+scanf("%d"&a);
+for(i=1;i<=a;i++)
+{
+sum=sum+i;
+}
+printf("The sum of natural numbers %d",sum);
+}
+getch();
+}
