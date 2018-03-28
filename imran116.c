561f63d3b5fb07e70807113bfa636c65e1ab955d
mohammedimrankhan786 <35915794+mohammedimrankhan786@users.noreply.github.com>
2018-03-28 15:59:18 +0530

    Update and rename imran115.c to imran116.c

---
+++ imran116.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int number,anumber;
+    printf("enter the number");
+    scanf("%d%d",&number,&anumber);
+    printf("%d%d",number,anumber);
+    return 0;
+}
+getch();
+}
